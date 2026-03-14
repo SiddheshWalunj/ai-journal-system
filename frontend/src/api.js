1d677097e1d264d4c3cf015c8b882e138bc09f0c
Mr.Siddhesh_Walunj <101124870+SiddheshWalunj@users.noreply.github.com>
2026-03-15 03:03:18 +0530

    files reuired for frontend

---
+++ frontend/src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL ;
+
+export const getEntries = userId =>
+  axios.get(`${API}/journal/${userId}`);
+
+export const saveEntry = data =>
+  axios.post(`${API}/journal`, data);
+
+export const analyzeEntry = text =>
+  axios.post(`${API}/journal/analyze`, { text });
+
+export const getInsights = userId =>
+  axios.get(`${API}/journal/insights/${userId}`);
