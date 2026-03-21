b855469c805a68b87fdefffffc4a7c9417216986
SiddheshWalunj <snwalunj007@gmail.com>
2026-03-22 02:24:54 +0530

    corrected file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 
 const API = process.env.REACT_APP_API_URL ;
@@ -14,20 +13,4 @@ export const analyzeEntry = text =>
 
 export const getInsights = userId =>
   axios.get(`${API}/journal/insights/${userId}`);
-=======
-import axios from "axios";
 
-const API = process.env.REACT_APP_API_URL ;
-
-export const getEntries = userId =>
-  axios.get(`${API}/journal/${userId}`);
-
-export const saveEntry = data =>
-  axios.post(`${API}/journal`, data);
-
-export const analyzeEntry = text =>
-  axios.post(`${API}/journal/analyze`, { text });
-
-export const getInsights = userId =>
-  axios.get(`${API}/journal/insights/${userId}`);
->>>>>>> 82864438c9d7d33b4bac508629d7c9e945d7e060
